2908e1b674a2793742fecdcef0626153d68c0643
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-07-15 10:54:53 -0500

    retriment info

--- schema.sql
+++ schema.sql
@@ -14,6 +14,14 @@ CREATE TABLE employees (
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no varchar(5) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -31,4 +39,52 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-SELECT * FROM departments;
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar(25) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL
+ );
+select *from departments
+select *from dept_emp
+select *from dept_manager
+select *from employees
+select *from salaries
+drop table titles;
+select *from titles
+
+--eligible for a retirement .
+-- born between 1952 and 1955.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+--only 1952 birth dates
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+--only born in 1953
+SELECT first_name,last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--create a table for retriment
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
