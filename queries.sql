90dc737441851b0cd09358aa866d71a3c3b274e0
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-07-15 18:12:44 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -167,3 +167,20 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
  AND (de.to_date = '9999-01-01')
 select *from emp_info
+
+--list for management department.
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+select*from manager_info
+	   
