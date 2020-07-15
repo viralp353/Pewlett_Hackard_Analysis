be77d3e7da800bcba52303fa9904ced183b65d19
viralp353 <65969608+viralp353@users.noreply.github.com>
2020-07-15 17:19:02 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -101,13 +101,13 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 SELECT * FROM retirement_info;
 
 -- Joining departments and dept_manager tables
-SELECT departments.dept_name,
-     dept_manager.emp_no,
-     dept_manager.from_date,
-     dept_manager.to_date
-FROM departments
-INNER JOIN dept_manager
-ON departments.dept_no = dept_manager.dept_no;
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
 
 -- Joining retirement_info and dept_emp tables
 SELECT retirement_info.emp_no,
@@ -117,4 +117,23 @@ SELECT retirement_info.emp_no,
 FROM retirement_info
 LEFT JOIN dept_emp
 ON retirement_info.emp_no = dept_emp.emp_no;
+
+SELECT ri.emp_no,
+	ri.first_name,
+    ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+--Use Left Join for retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+select *from current_emp
 
